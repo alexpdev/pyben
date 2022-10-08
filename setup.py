1a6756cfb78f14704fd96238ccd142acf784de1b
Alex <alexpdev@protonmail.com>
2022-10-07 22:07:28 -0700

    changed some CI/CD config files and bunp to 0.3.3

--- setup.py
+++ setup.py
@@ -20,39 +20,6 @@
 ############################################################################
 """Pyben setuptools information."""
 
-import json
-from setuptools import find_packages, setup
+from setuptools import setup
 
-INFO = json.load(open('./package.json', encoding='utf-8'))
-
-with open("README.md", "rt", encoding="UTF-8") as readme:
-    long_description = readme.read()
-
-setup(
-    name=INFO['name'],
-    version=INFO['version'],
-    description=INFO['description'],
-    long_description=long_description,
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-    ],
-    keywords=INFO["keywords"],
-    author=INFO["author"],
-    author_email=INFO["email"],
-    url=INFO["url"],
-    project_urls={"Source Code": "https://github.com/alexpdev/pyben"},
-    license=INFO["license"],
-    packages=find_packages(exclude=["tests", "env"]),
-    include_package_data=True,
-    tests_require=["pytest"],
-    setup_requires=["setuptools"],
-    zip_safe=False,
-    test_suite="complete",
-)
+setup()
